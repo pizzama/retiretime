38a0cd2ee5d98e33ac7448010be45284a4bbd716
bin.sun <bin.sun@fh.com>
2025-03-05 02:32:03 +0800

    Initial commit

---
+++ retiretime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  retiretime
+//
+//  Created by pizzaman on 2025/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ retiretime/retiretimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  retiretimeApp.swift
+//  retiretime
+//
+//  Created by pizzaman on 2025/3/5.
+//
+
+import SwiftUI
+
+@main
+struct retiretimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
